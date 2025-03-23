e6b50059f6f950b4792434656d55ad6d6e323663
oussama7007 <oussamatachakourt@gmail.com>
2025-03-23 09:07:42 +0000

    D

--- src/parse.c
+++ src/parse.c
@@ -6,7 +6,7 @@
 /*   By: oait-si- <oait-si-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/03 13:00:51 by oait-si-          #+#    #+#             */
-/*   Updated: 2025/03/22 21:41:42 by oait-si-         ###   ########.fr       */
+/*   Updated: 2025/03/23 07:03:56 by oait-si-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -273,12 +273,12 @@ void free_dubel(char **str, int height)
 }
 void    flood_fill(t_map *map, char **grid, int x, int y)
 {
-    if(y >= map->height || x >= map->width || grid[y][x] == '1')
+    if(y >= map->height || x >= map->width || grid[y][x] == '1' )
         return;
-    if(grid[y][x] == 'E')
+    if(grid[y][x] == 'E' )
     {
        map->exit_reachable++;
-      // return;
+
     }
     if(grid[y][x] == 'C')
         map->c_collected++;
@@ -288,12 +288,46 @@ void    flood_fill(t_map *map, char **grid, int x, int y)
     flood_fill(map, grid, x, y + 1);
     flood_fill(map, grid, x, y - 1);
 }
+
+void    flood_fill_1(t_map *map, char **grid, int x, int y)
+{
+    if(y >= map->height || x >= map->width || grid[y][x] == '1' )
+        return;
+    if(grid[y][x] == 'E' )
+    {
+        grid[y][x] = '1';
+    }
+    if(grid[y][x] == 'C')
+        map->c_collected++;
+    grid[y][x] = '1';
+    flood_fill(map, grid, x + 1, y);
+    flood_fill(map, grid, x - 1, y);
+    flood_fill(map, grid, x, y + 1);
+    flood_fill(map, grid, x, y - 1);
+}
+int check_path_to_c(t_map *map)
+{
+    char **test;
+
+    test = mimc_map(map);
+    if(!test)
+        return 0;
+    map->exit_reachable = 0;
+    map->c_collected = 0;
+    flood_fill_1(map, test, map->player.x, map->player.y);
+    free_dubel(test, map->height);
+    
+    return(map->c_collected ==  map->c_count);
+}
 int validate_components(t_map *map)
 {
     char **new;
     
+    
     new = mimc_map(map);
     if(!new)
+        return 0;
+    if(!check_path_to_c(map))
         return 0;
     map->exit_reachable = 0;
     map->c_collected = 0;
