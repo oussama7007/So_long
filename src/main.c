547ad987450d9f3307c94bbbba41a5a3efd2d3a5
oussama7007 <oussamatachakourt@gmail.com>
2025-04-29 20:34:58 +0100

    done

--- src/main.c
+++ src/main.c
@@ -6,18 +6,14 @@
 /*   By: oait-si- <oait-si-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/28 08:53:58 by oait-si-          #+#    #+#             */
-/*   Updated: 2025/04/20 17:39:25 by oait-si-         ###   ########.fr       */
+/*   Updated: 2025/04/29 20:34:16 by oait-si-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "so_long.h"
-void f()
-{
-	system("leaks so_long");
-}
+
 int	main(int ac, char **av)
 {
-	atexit(f);
 	t_game	game;
 
 	//ft_memset(&game, 0, sizeof(t_game));
